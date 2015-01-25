5d9c62524f2d5c44e898a368f6cd018fcae11e48
rberangi <rberangi@gmail.com>
2015-01-25 08:21:29 +0330

    Assignment for week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix is an R function that gets a matrix and
+## returns a named list containing 3 functions:
+## 	1. getMatrix= a function to get the matrix itself
+## 	2. setInverse= A function  to update the cached matrix
+##       inverse with the already computed inverse 
+## 	3. getMatrixInverse= A function  to show the matrix inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
+        Inversecache <- NULL
+        getMatrix <- function() x 
+        setInverse <- function(Inverse) Inversecache <<- Inverse
+        getMatrixInverse <- function() Inversecache
+        list(getMatrix = getMatrix, setInverse = setInverse,
+		 getMatrixInverse = getMatrixInverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is an R function that gets the list provided by
+## the makeCacheMatrix function and
+## 1. checks weather the matrix inverse exist
+## 2. if not! computes the matrix inverse and updates the inverse cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(y) {
+	## step 1: get the matrix inverse cache
+ 
+      Inversecache <- y$getMatrixInverse ()
+
+      if(!is.null(Inversecache )){
+           message("Matrix cache exist and though returned")
+           return(Inversecache)
+    	     }
+	else {
+        	message("Inverse does not exist and to be copmuted")
+        	Matrix <- y$getMatrix() 
+        	Inversecache <- solve(Matrix) 
+        	y$setInverse (Inversecache )  ## update the cache value
+        	return(Inversecache )
+    		}
 }
